d3507275cb7429acf229c9df29143efadc62e6c6
facuperezbri <facundo.perez.brizuela@gmail.com>
2022-06-22 16:07:53 -0300

    api get complete

--- api/src/routes/recipes.js
+++ api/src/routes/recipes.js
@@ -1,12 +1,38 @@
+require("dotenv").config();
+const { API_KEY } = process.env;
 const { Router } = require("express");
 const router = Router();
+const axios = require("axios");
+const { Recipe, Diet } = require("../db");
+const db = require("../db");
 
-router.get("/", (req, res, next) => {
+let apiURL = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}&addRecipeInformation=true&number=100`;
+
+router.get("/", async (req, res, next) => {
 	const { name } = req.query;
-	if (name) {
-		res.send(`Hola, soy ${name} de req.query`);
+
+	try {
+		let apiPromiseData = await axios.get(apiURL);
+		let dbPromiseData = await Recipe.findAll({
+			include: Diet,
+		});
+
+		Promise.all([apiPromiseData, dbPromiseData]).then((respuesta) => {
+			const [apiPromisedData, dbData] = respuesta;
+			let apiData = apiPromisedData.data.results.map((r) => {
+				return {
+					id: r.id,
+					name: r.title,
+					summary: r.summary,
+					healthScore: r.healthScore,
+					steps: r.steps,
+				};
+			});
+			res.send(apiData);
+		});
+	} catch (error) {
+		next(error);
 	}
-	res.send("Hola soy /get de recipes");
 });
 
 router.get("/:idReceta", (req, res, next) => {
@@ -14,10 +40,19 @@ router.get("/:idReceta", (req, res, next) => {
 	res.send(`Hola, soy ${idReceta} de req.params`);
 });
 
-router.post("/", (req, res, next) => {
+router.post("/", async (req, res, next) => {
 	const { name, summary, healthScore, steps } = req.body;
-	res.send(`Yo traigo los valores de body
-    name: ${name}`);
+	try {
+		let newRecipe = await Recipe.create({
+			name,
+			summary,
+			healthScore,
+			steps,
+		});
+		res.status(201).send(newRecipe);
+	} catch (error) {
+		next(error);
+	}
 });
 
 module.exports = router;
