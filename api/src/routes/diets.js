86f58ec3a5ef9eaaa5cfeb5564635b344870a7e1
facuperezbri <facundo.perez.brizuela@gmail.com>
2022-06-25 15:08:01 -0300

    Update

--- api/src/routes/diets.js
+++ api/src/routes/diets.js
@@ -1,8 +1,35 @@
 const { Router } = require("express");
 const router = Router();
+const { Diet } = require("../db");
 
-router.get("/", (req, res, next) => {
-	res.send("Soy el get de Diets");
+router.get("/", async (req, res, next) => {
+	const diets = [
+		"Gluten Free",
+		"Ketogenic",
+		"Vegetarian",
+		"Lacto-Vegetarian",
+		"Ovo-Vegetarian",
+		"Vegan",
+		"Pescetarian",
+		"Paleo",
+		"Primal",
+		"Low FODMAP",
+		"Whole30",
+	];
+	try {
+		diets.forEach((d) => {
+			Diet.findOrCreate({
+				where: {
+					name: d,
+				},
+			});
+		});
+
+		const allDiets = await Diet.findAll();
+		res.send(allDiets);
+	} catch (error) {
+		next(error);
+	}
 });
 
 module.exports = router;
